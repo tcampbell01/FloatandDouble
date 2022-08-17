c853693ac0262886635b065fb0585a340780d350
tcampbell01 <teresa.j.campbell@gmail.com>
2022-08-17 15:55:44 -0400

    int float double

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,27 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello crazy people!");
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5;
+        //floats aren't recommended to be used much anymore
+        float myFloatValue = 5.25f;
+        double myDoubleValue = 5d;
+
+
+
+
+
+    }
+}
