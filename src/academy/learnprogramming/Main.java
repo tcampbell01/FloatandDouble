a650b9f65bcc662da185e99375ef58462277a4d3
tcampbell01 <teresa.j.campbell@gmail.com>
2022-08-17 15:59:38 -0400

    double vs float

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -14,12 +14,16 @@ public class Main {
         System.out.println("Double minimum value = " + myMinDoubleValue);
         System.out.println("Double maximum value = " + myMaxDoubleValue);
 
-        int myIntValue = 5;
+        int myIntValue = 5/3; // this will only give us the whole number associated with int = 2
         //floats aren't recommended to be used much anymore
-        float myFloatValue = 5.25f;
-        double myDoubleValue = 5d;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d/ 3d;
 
+        //double is much more precise than float.  double will go out 16 places.  float only 8 places.
 
+    System.out.println("MyIntValue = " + myIntValue);
+    System.out.println("MyFloatValue = " + myFloatValue);
+    System.out.println("MyDoubleValue = " + myDoubleValue);
 
 
 
